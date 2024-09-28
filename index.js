5a20352fef0320a111ae9a961112659d14fa32ed
Racheal <rachealnjugush@gmail.com>
2024-09-28 23:11:02 +0300

    arroe functions

---
+++ index.js
@@ -0,0 +1,9 @@
+//test1
+function divide(parameter1, parameter2){
+    return parameter1/parameter2;
+}
+divide(2000/100);
+
+//test2
+const square = parameter1 => parameter1 ** parameter1;
+const add = (parameter1, parameter2) => parameter1 + parameter2;
